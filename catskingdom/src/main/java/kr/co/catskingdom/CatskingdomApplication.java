3929bb393d1e2179beaa61917bee846f1f1d5de4
tkdenddl1324 <tkdenddl1324@gmail.com>
2025-04-11 09:13:09 +0900

    layout

---
+++ catskingdom/src/main/java/kr/co/catskingdom/CatskingdomApplication.java
@@ -0,0 +1,13 @@
+package kr.co.catskingdom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatskingdomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatskingdomApplication.class, args);
+    }
+
+}
---
+++ catskingdom/src/test/java/kr/co/catskingdom/CatskingdomApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.catskingdom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatskingdomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
